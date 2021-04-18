1f1f4dcf2e39a41cd45f14eba3ac913e5432d6b4
praanggi09 <praanggi09@gmail.com>
2021-04-18 09:41:59 +0700

    initial commit

---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,35 @@
+<header class="container text-center">
+  <a class="navbar-brand text-dark" href="#" style="font-family: Poppins; font-size: 70px">.maleskerja</a>
+</header>
+
+<nav class="navbar navbar-expand-lg navbar-light align-text-center" style="font-family: Poppins">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+    aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <section class="container">
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav mx-auto border-top">
+        <li class="nav-item mr-5">
+          <a class="nav-link active" href="#">HOME</a>
+        </li>
+        <li class="nav-item mr-5">
+          <a class="nav-link" href="#">SERVICES</a>
+        </li>
+        <li class="nav-item mr-5">
+          <a class="nav-link" href="#">PORTOFOLIO</a>
+        </li>
+        <li class="nav-item mr-5">
+          <a class="nav-link" href="#">JOURNAL</a>
+        </li>
+        <li class="nav-item mr-5">
+          <a class="nav-link" href="#">CONTACT</a>
+        </li>
+        <li class="nav-item ml-auto">
+          <a class="nav-link" href="#">TEAM</a>
+        </li>
+      </ul>
+    </div>
+  </section>
+</nav>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('title', 'Branding Consultant, Branding Agency')
+
+@section('content')
+<header class="mb-3 hero text-right py-5">
+
+  <h1 class=" container text-light align-content-center mt-5" style="font-weight: lighter">
+    The Art of Building
+    <br />
+    Sustainable Brands.
+  </h1>
+</header>
+
+<div class="container">
+  <div class="row my-5">
+    <div class="col-6 my-5">
+
+    </div>
+  </div>
+</div>
+@endsection
